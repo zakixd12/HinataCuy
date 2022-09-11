75c71d1969c7c680ced7e3bc4b27958f603136eb
zakixd12 <zakiarlop1927@gmail.com>
2022-09-11 19:42:51 +0700

    s

---
+++ settings.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Free Apikey
+global.APIs = {
+	riy: 'https://api-xriy04.herokuapp.com',
+}
+
+// Other
+global.owner = ['6289506219231']
+global.premium = ['6289506219231']
+global.youtube = 'https://youtube.com/c/EVOS ARULL'
+global.ownername = 'JACKY'
+global.botname ='Patrik-Md'
+global.donasi = {
+    saweria: 'https://saweria.co/Jacky',
+    nomor: '089506219231'
+}
+global.packname = 'Patrik-Md'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...'
+}
+global.thumb = fs.readFileSync('./image/hinata.jpg')
+global.vn = './sound/menu.mp3'
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
